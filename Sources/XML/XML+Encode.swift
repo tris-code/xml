4ecba383eee14b3e9a116f7e74293ccb10e32e70
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-10-30 17:41:59 +0000

    [Decode] Allow uppercased header

--- Sources/XML/XML+Encode.swift
+++ Sources/XML/XML+Encode.swift
@@ -11,23 +11,6 @@
 
 import Stream
 
-extension XML.Encoding {
-    var rawValue: String {
-        switch self {
-        case .utf8: return "utf-8"
-        }
-    }
-}
-
-extension XML.Standalone {
-    var rawValue: String {
-        switch self {
-        case .yes: return "yes"
-        case .no: return "no"
-        }
-    }
-}
-
 // TODO: Implement and benchmark raw encoder
 
 extension XML.Document {
